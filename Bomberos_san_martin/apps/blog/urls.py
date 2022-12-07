c82796e4feebe0a662670a9412e485dd6fd44af4
RamaAlves <ramiroalvesprof@outlook.com>
2022-12-07 11:34:39 -0300

    Creando archivos html estaticos

--- Bomberos_san_martin/apps/blog/urls.py
+++ Bomberos_san_martin/apps/blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 app_name = "blog"
 urlpatterns = [
-    #path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('contacto', views.contacto, name='contacto'),
+    path('aspirantes', views.aspirantes, name='aspirantes'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('perfil', views.perfil, name='perfil'), 
 ]
--- Bomberos_san_martin/apps/blog/views.py
+++ Bomberos_san_martin/apps/blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+
+    return render(request, 'index.html',)
+
+def contacto(request):
+
+    return render(request, 'contacto.html',)
+
+def aspirantes(request):
+
+    return render(request, 'aspirantes.html',)
+
+def nosotros(request):
+
+    return render(request, 'nosotros.html',)
+
+def perfil(request):
+
+    return render(request, 'perfil.html',)
