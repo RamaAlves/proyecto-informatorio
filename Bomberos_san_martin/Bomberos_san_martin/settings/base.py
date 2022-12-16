8a5224afe5096242fcc407cc3926d79a124df6fe
RamaAlves <ramiroalvesprof@outlook.com>
2022-12-16 01:58:54 -0300

    Agregando funciones restaurar contraseña y cambiar contraseña

--- Bomberos_san_martin/Bomberos_san_martin/settings/base.py
+++ Bomberos_san_martin/Bomberos_san_martin/settings/base.py
@@ -130,3 +130,11 @@ LOGOUT_REDIRECT_URL= 'blog:index'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#CONFIGURACION DE EMAIL
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "bomberos.san.martin.6@gmail.com"
+EMAIL_HOST_PASSWORD = 'ywzqijkrqbqbvgnh'
--- Bomberos_san_martin/apps/blog/urls.py
+++ Bomberos_san_martin/apps/blog/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path
-
+from django.urls import path, reverse_lazy
+from django.contrib import admin
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, 
+    PasswordResetConfirmView,PasswordResetCompleteView,PasswordChangeView,PasswordChangeDoneView)
 
 app_name = "blog"
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path('', views.index, name='index'),
     path('contacto/', views.contacto, name='contacto'),
     path('nosotros/', views.nosotros, name='nosotros'),
@@ -37,6 +40,16 @@ urlpatterns = [
     path('administrar_bomberos/', views.administrarBomberos, name='administrar_bomberos'),
     path('agregar_bombero/', views.agregarBombero, name='agregar_bombero'),
     path('actualizar_bombero/<int:pk>', views.actualizarBombero, name='actualizar_bombero'),
-    path('eliminar_bombero/<int:pk>', views.eliminarBombero, name='eliminar_bombero')
+    path('eliminar_bombero/<int:pk>', views.eliminarBombero, name='eliminar_bombero'),
+
+    path('reset_password/', PasswordResetView.as_view(template_name='resetpass/password_reset_form.html',success_url=reverse_lazy('blog:password_reset_done'), email_template_name='resetpass/password_reset_email.html'), name='password_reset'),
+    path('reset_password_send/', PasswordResetDoneView.as_view(template_name='resetpass/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(template_name='resetpass/password_reset_confirm.html',success_url=reverse_lazy('blog:password_reset_complete')), name='password_reset_confirm'),
+    path('reset_password_complete/', PasswordResetCompleteView.as_view(template_name='resetpass/password_reset_complete.html'), name='password_reset_complete'),
+    
+    path('cambiar_contraseña/', PasswordChangeView.as_view(template_name='resetpass/password_change_form.html',success_url=reverse_lazy('blog:password_change_done')), name='password_change'),
+    path('cambiar_contraseña_completo/', PasswordChangeDoneView.as_view(template_name='resetpass/password_change_done.html'), name='password_change_done')
+
+
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
